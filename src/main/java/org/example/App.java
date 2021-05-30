23eaba4e1e1c1dc84bd31bcf40b3b60fd6bdb82f
ryanpersad <Rpersad0305@gmail.com>
2021-05-30 00:17:10 -0400

    new

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,11 +7,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        //Initializes Scanner
         Scanner myObj = new Scanner(System.in);
         System.out.println( "Enter the quotes" );
+        //Input
         String Quotes =  myObj.nextLine();
+        //Input
         System.out.println( "Who said it?" );
         String author =  myObj.nextLine();
+        //Output
         System.out.println( author + " said," +"\"" + Quotes +"\"" );
     }
 }

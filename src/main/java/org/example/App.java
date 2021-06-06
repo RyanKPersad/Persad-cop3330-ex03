e0f8e18342ef89ef22e9dbc69b676c4a1136e19e
ryanpersad <Rpersad0305@gmail.com>
2021-06-06 19:42:57 -0400

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Persad
+ */
+
 package org.example;
 import java.util.Scanner;
-/**
- *
- */
+
+
 public class App 
 {
     public static void main( String[] args )
